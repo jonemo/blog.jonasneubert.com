bb02e79c2d3b1f47b35fa46a331ea3305e25dbe6
aabidsofi19 <65964225+aabidsofi19@users.noreply.github.com>
2022-09-14 09:14:34 +0530

    upwork project part 3  (#7)
    * Merge commit '9239' clean up css
    * added prose to index list and removed nested main

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,51 @@ module.exports = {
     'partials/*.html',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: [
+        'Quicksand',
+        'Century Gothic',
+        'AppleGothic',
+        'ui-sans-serif',
+        'system-ui',
+        'Helvetica',
+        'sans-serif',
+      ],
+
+      monospace: ['Cutive Mono', 'monospace'],
+    },
+
+    extend: {
+      colors: {
+        red: {
+          600: '#c40d0b',
+        },
+        yellow: {
+          600: '#ffe400',
+        },
+      },
+
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            code: {
+              backgroundColor: theme('colors.gray.100'),
+              color: theme('colors.gray.800'),
+              fontWeight: '400',
+              'border-radius': '0.25rem',
+            },
+            'code::before': {
+              content: '""',
+              'padding-left': '0.25rem',
+            },
+            'code::after': {
+              content: '""',
+              'padding-right': '0.25rem',
+            },
+          },
+        },
+      }),
+    },
   },
   plugins: [typography],
 }
